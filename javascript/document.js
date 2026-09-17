8fa87ab40f8ffd2254615808d9cc2f731f86d5e2
Amira-30 <amiraabdelhamid771@gmail.com>
2026-09-17 19:17:53 +0300

    update

--- javascript/document.js
+++ javascript/document.js
@@ -8,3 +8,16 @@ const copy= {...orignal,setting:{...orignal.setting}}
 copy.setting.theme= "light";
 console.log(orignal.setting.theme);
 
+
+let x= 3; 
+let y= x++;
+console.log(x, y);
+
+
+let a = 10;
+if( a = 5){
+    console.log("hello");
+    
+}
+
+
